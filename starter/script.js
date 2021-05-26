110f4e323bfc3ac42cfc5d78bbabbb989b39e467
hanipnk <hanipnk@gmail.com>
2021-05-26 17:56:20 -0400

    Working with Strings

--- starter/script.js
+++ starter/script.js
@@ -59,6 +59,179 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+// Split and Join
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  //console.log(names);  // --> ["jessica", "ann", "smith", "davis"]
+  const namesUpper = [];
+  for (const n of names) {
+    //namesUpper.push(n[0].toUpperCase() + n.slice(1));    ----> same result as one below
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  //console.log(namesUpper); // ---> ["Jessica", "Ann", "Smith", "Davis"]
+  console.log(namesUpper.join(' '));
+};
+capitalizeName('jessica ann smith davis');
+capitalizeName('jonas schmedtmann');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+')); // ---> 25 = 'length of strings' , '+' = 'adding '+' to fill up with'
+console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + ''; // if one of the operands of plus sign is a string, it will convert all the operands to a string
+  //const str = String(number) ---> same as one above
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+console.log(maskCreditCard(456789123));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('45643213654654654564'));
+
+// Repeat
+const message2 = 'Bad weather... All Departures Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line${'🔥'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
+// Working with Strings - Part 2
+const airline = 'TAP Air Portugal';
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+console.log('jonas'.toUpperCase());
+
+//Fix capitalization in name
+const passenger = 'jOnAS'; // Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+const correctNames = function (names) {
+  const lowerName = names.toLowerCase();
+  const lastName =
+    lowerName[0].toUpperCase() + lowerName.slice(1, lowerName.indexOf(',')); //----> does not incluse ','
+  const firstName = lowerName.slice(lowerName.indexOf(','));
+  const capfirstName = firstName[1].toUpperCase() + firstName.slice(2);
+  console.log(`${lastName},${capfirstName}`);
+
+  //return console.log(lastName + capfirstName);
+  // firstName = firstName[0].toUpperCase;
+  //console.log(lastName,firstName);
+  //return console.log(finalNames);
+};
+correctNames('park,heeyoung');
+correctNames('o,woong');
+correctNames('park,seoju');
+
+// Comparing email
+const email = 'hello@jonas.io';
+const loginEmail = '  Hello@Jonas.Io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim(); //  ----> removing the spaces and '\'
+// console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// Replacing
+const priceGB = '288,97$';
+const priceUS = priceGB.replace('$', '&').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replaceAll('door', 'gate'));
+
+console.log(announcement.replace(/door/g, 'gate')); // 'g' stands for global to change all 'door'occurance to gate
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Ai'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+//Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome on board');
+  }
+};
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+*/
+
+/*
+//Working with Strings - Part1
+const airline = `TAP Air Portugal`;
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('8737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7)); // length of string will be 'end -' 'beginning' = '3'
+// it contains 4 to 6  DOES NOT include 7
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+console.log(airline.slice(-1));
+
+const checkMiddleSeat = function (seat) {
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat');
+  else console.log('You got lucky');
+};
+//B and E are middle seats
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('jonas'));
+console.log(typeof new String('jonas')); //----> Object
+console.log(typeof new String('jonas').slice(1)); // --->'onas'
+
+*/
 
 /*
 
@@ -110,7 +283,6 @@ for (const [min, event] of gameEvents) {
 }
 
 */
-
 /*
 
 // Map : Iteration
